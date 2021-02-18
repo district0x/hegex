afd2e1546d548044c81fcfee14810bbf37dc0e47
stacksideflow <1@stacksideflow.host>
2021-02-18 18:43:32 +0300

    [P&L] Complete asset price fetcher

--- src/district_hegex/ui/external/events.cljs
+++ src/district_hegex/ui/external/events.cljs
@@ -1,15 +1,42 @@
 (ns district-hegex.ui.external.events
   (:require
-   [re-frame.core :as re-frame]))
-
+   [cljs.core.async :refer [go]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+   [oops.core :refer [gcall ocall oget]]
+   [re-frame.core :as re-frame :refer [dispatch]]))
 
 (def interceptors [re-frame/trim-v])
 
-(re-frame/reg-event-db
+(def ^:private base-uri "https://api.coingecko.com/api/v3/coins/")
+
+(def ^:private asset-to-fetch "ethereum")
+
+(re-frame/reg-event-fx
   ::fetch-asset-prices
   interceptors
-  (fn [db _]
-    (update-in db [::dark-mode?] not)))
+  (fn [_ _]
+    {::fetch-asset-prices! true}))
+
+(re-frame/reg-event-db
+  ::fetch-asset-prices-success
+  interceptors
+  (fn [db [price]]
+    (assoc-in db [:prices :eth] price)))
+
+(defn fetch-asset-prices []
+  (go
+    (try
+      (dispatch [::fetch-asset-prices-success
+                 (oget (<p! (ocall (<p! (gcall "fetch"
+                                           (str base-uri asset-to-fetch))) "json"))
+            ".?market_data.?current_price.?usd")])
+      (catch js/Error err (js/console.log (ex-cause err))))))
+
+(re-frame/reg-fx
+  ::fetch-asset-prices!
+  (fn []
+    (fetch-asset-prices)))
+
 
 ;; fetch for P&L
 ;; (await(await fetch("https://api.coingecko.com/api/v3/coins/ethereum")).json()).market_data.current_price.usd
