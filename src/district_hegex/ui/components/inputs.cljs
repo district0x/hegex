2b3b6d35e583853e2143ba321867c095f7142da0
stacksideflow <1@stacksideflow.host>
2021-02-25 18:22:11 +0300

    Fix label dark mode bg inheritance

--- src/district_hegex/ui/components/inputs.cljs
+++ src/district_hegex/ui/components/inputs.cljs
@@ -1,4 +1,7 @@
-(ns district-hegex.ui.components.inputs)
+(ns district-hegex.ui.components.inputs
+(:require
+   [district-hegex.ui.home.subs :as home-subs]
+   [re-frame.core :refer [subscribe]]))
 
 (defn- little-arrow []
   [:span.little-arrow
@@ -11,11 +14,13 @@
     (into [:select] children)]])
 
 (defn text-input [{:keys [type min max on-change label]}]
-  [:div.hinput-wrapper
-   [:input.hegex-input {:type type
-                        :on-change on-change
-                        :min min
-                        :max max}]
-   (when label
-     [:div.hinput-label
-      label])])
+  (let [dark? @(subscribe [::home-subs/dark-mode?])]
+    [:div.hinput-wrapper
+    [:input.hegex-input {:type type
+                         :on-change on-change
+                         :min min
+                         :max max}]
+    (when label
+      ;;NOTE ideally  should be set via less inheritance
+      [:div.hinput-label {:style {:background-color (if dark? "black" "white")}}
+       label])]))
