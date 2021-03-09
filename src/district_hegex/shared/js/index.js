74f3a9d338ea28185e98a9a6045d150fbf5ec3ea
stacksideflow <1@stacksideflow.host>
2021-03-09 18:21:09 +0300

    Remove blueprint

--- src/district_hegex/shared/js/index.js
+++ src/district_hegex/shared/js/index.js
@@ -1,8 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
 const Web3 = require('web3');
-// import Web3 from 'web3';
-// import { SnackbarProvider, withSnackbar } from 'notistack';
 
 import * as Connect0x from '@0x/connect';
 import * as Contract0x from '@0x/contract-wrappers';
@@ -11,21 +7,8 @@ import * as Utils0x from '@0x/utils';
 import * as Web3Wrapper0x from '@0x/web3-wrapper';
 import * as Subproviders0x from '@0x/subproviders';
 import * as Addresses0x from "@0x/contract-addresses";
-// // style
-// import * as Rebass from 'rebass';
-// import * as RebassPreset from '@rebass/preset';
-// import * as Emotion from '@emotion/react'
-// import * as RebassForms from '@rebass/forms';
-// import * as Styled from 'styled-components';
-// import * as Blueprint from "@blueprintjs/core";
-// import * as BlueprintIcons from "@blueprintjs/icons";
-
-// import "@blueprintjs/core/lib/css/blueprint.css";
-// import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 
-// window.React = React;
-// window.ReactDOM = ReactDOM;
 window.Connect0x = Connect0x;
 window.Contract0x = Contract0x;
 window.OrderUtils0x = OrderUtils0x;
@@ -34,16 +17,3 @@ window.Subproviders0x = Subproviders0x;
 window.Web3Wrapper0x = Web3Wrapper0x;
 window.Web3x = Web3;
 
-// window.Rebass = Rebass;
-// window.RebassPreset = RebassPreset;
-// window.Emotion = Emotion;
-// window.RebassForms = RebassForms;
-// window.Styled = Styled;
-// window.Blueprint = Blueprint;
-// window.BlueprintIcons = BlueprintIcons;
-
-
-// //useless
-// window.withSnackbar = withSnackbar;
-// window.StackedSnackbars = SnackbarProvider;
-// window.Addresses0x = Addresses0x;
