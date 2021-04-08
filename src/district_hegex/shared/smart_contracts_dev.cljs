9ec7c7f2e32ddb09aff59baf1cd159458736e828
stacksideflow <1@stacksideflow.host>
2021-04-08 14:09:37 +0300

    Update dev contracts

--- src/district_hegex/shared/smart_contracts_dev.cljs
+++ src/district_hegex/shared/smart_contracts_dev.cljs
@@ -1,9 +1,9 @@
 (ns district-hegex.shared.smart-contracts-dev)
 
 (def smart-contracts
-  {:optionchef {:name "OptionChef" :address "0xe3c55a753f9de0bfe42010d48bbe34010d7766d9"}
-   :hegexoption {:name "Hegexoption" :address "0x591b42d573c293c58e22242239f58c251f19dc9f"}
-   :optionchefdata {:name "OptionChefData" :address "0x8c586cf3ca4e9aa114cb60b3512ffe8ec019426e"}
+  {:optionchef {:name "OptionChef" :address "0x9c6849af85ec10c385728da68be00aa7a9fd1bdd"}
+   :hegexoption {:name "Hegexoption" :address "0x7d51ae46716c96ee46bd4be7a9ff65026d1ac08e"}
+   :optionchefdata {:name "OptionChefData" :address "0x9a2eafa10a076751e2ad1493a6178dc4876cffd8"}
    ;; NOTE external contracts down below
    :weth {:name "WETH" :address "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"}
    :wbtcoptions {:name "HegicWBTCOptions" :address "0x3961245DB602eD7c03eECcda33eA3846bD8723BD"}
