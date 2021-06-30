c7ebc4936ffe3385030e2ae2a52c9e95c9bd17ae
stacksideflow <1@stacksideflow.host>
2021-07-01 00:41:02 +0300

    Redeploy metadataV2 contracts

--- src/district_hegex/shared/smart_contracts_qa.cljs
+++ src/district_hegex/shared/smart_contracts_qa.cljs
@@ -1,9 +1,9 @@
 (ns district-hegex.shared.smart-contracts-qa)
 
 (def smart-contracts
-  {:optionchef {:name "OptionChef" :address "0x5dbb39b7685237723ad54854398a3f435aafa629"}
-   :hegexoption {:name "Hegexoption" :address "0x504f637fa976fba9b079c3cb6c723e92abe4059d"}
-   :optionchefdata {:name "OptionChefData" :address "0x0c3322625733d4b02c843b34834c7e8d7cdc4f30"}
+  {:optionchef {:name "OptionChef" :address "0xf220c7c0c465814d6947a3f4805c1349a2c43132"}
+   :hegexoption {:name "Hegexoption" :address "0xfc2080038b80f02c7cfd7a308335571de3568ad0"}
+   :optionchefdata {:name "OptionChefData" :address "0xccb98418f4b7d68bb7d051b823eb275c46f0ebc6"}
    :weth {:name "WETH" :address "0xc778417E063141139Fce010982780140Aa0cD5Ab"}
    :wbtcoptions {:name "HegicWBTCOptions" :address "0x3aD466588F5f8f1Ce896645d5322db3F25810639"}
    :brokenethoptions {:name "BrokenETHOptions" :address "0x77041D13e0B9587e0062239d083b51cB6d81404D"}
