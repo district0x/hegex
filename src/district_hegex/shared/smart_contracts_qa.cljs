c3ce7718ed8e5c12db4d46b9b77d76964c884710
stacksideflow <1@stacksideflow.host>
2021-06-30 23:20:33 +0300

    Redeploy fixed ropsten contracts

--- src/district_hegex/shared/smart_contracts_qa.cljs
+++ src/district_hegex/shared/smart_contracts_qa.cljs
@@ -1,9 +1,9 @@
 (ns district-hegex.shared.smart-contracts-qa)
 
 (def smart-contracts
-  {:optionchef {:name "OptionChef" :address "0x7d7ba91e1754e0d89610329eef79d646be13b68c"}
-   :hegexoption {:name "Hegexoption" :address "0x6e4674a9573daafd6c706ff854fe1d73ce8f082d"}
-   :optionchefdata {:name "OptionChefData" :address "0xecb236720a009024a50365072d71c39b116da176"}
+  {:optionchef {:name "OptionChef" :address "0x5dbb39b7685237723ad54854398a3f435aafa629"}
+   :hegexoption {:name "Hegexoption" :address "0x504f637fa976fba9b079c3cb6c723e92abe4059d"}
+   :optionchefdata {:name "OptionChefData" :address "0x0c3322625733d4b02c843b34834c7e8d7cdc4f30"}
    :weth {:name "WETH" :address "0xc778417E063141139Fce010982780140Aa0cD5Ab"}
    :wbtcoptions {:name "HegicWBTCOptions" :address "0x3aD466588F5f8f1Ce896645d5322db3F25810639"}
    :brokenethoptions {:name "BrokenETHOptions" :address "0x77041D13e0B9587e0062239d083b51cB6d81404D"}
