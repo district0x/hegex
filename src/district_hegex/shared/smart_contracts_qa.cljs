ecbe85ac360e78d3f42ec7c24534aa8605d1b00b
stacksideflow <1@stacksideflow.host>
2021-06-30 21:38:32 +0300

    Update contracts to latest

--- src/district_hegex/shared/smart_contracts_qa.cljs
+++ src/district_hegex/shared/smart_contracts_qa.cljs
@@ -1,9 +1,9 @@
 (ns district-hegex.shared.smart-contracts-qa)
 
 (def smart-contracts
-  {:optionchef {:name "OptionChef" :address "0xa4d65ad4478a5216071f3cfb73c3a3ad5bc6e8a2"}
-   :hegexoption {:name "Hegexoption" :address "0x42b49d4b14411c40243d02dee86abb7157b28e34"}
-   :optionchefdata {:name "OptionChefData" :address "0x4b162F735b9f32892CF0730AFe1676da50DE50A8"}
+  {:optionchef {:name "OptionChef" :address "0x7d7ba91e1754e0d89610329eef79d646be13b68c"}
+   :hegexoption {:name "Hegexoption" :address "0x6e4674a9573daafd6c706ff854fe1d73ce8f082d"}
+   :optionchefdata {:name "OptionChefData" :address "0xecb236720a009024a50365072d71c39b116da176"}
    :weth {:name "WETH" :address "0xc778417E063141139Fce010982780140Aa0cD5Ab"}
    :wbtcoptions {:name "HegicWBTCOptions" :address "0x3aD466588F5f8f1Ce896645d5322db3F25810639"}
    :brokenethoptions {:name "BrokenETHOptions" :address "0x77041D13e0B9587e0062239d083b51cB6d81404D"}
